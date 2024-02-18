1a8f1f5c126f46006f19d6484c4f3fccdef86b95
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2024-02-18 12:43:06 +0100

    bozza DB structure + modifiche UML

--- proj_eclipse/db/config/db_creation.sql
+++ proj_eclipse/db/config/db_creation.sql
@@ -1,14 +1,58 @@
-CREATE TABLE IF NOT EXISTS Corsia (id INTEGER PRIMARY KEY);
-
-CREATE TABLE IF NOT EXISTS Scaffale (
-    id INTEGER,
-    corsia INTEGER REFERENCES Corsia(id),
-    PRIMARY KEY(id, corsia)
+CREATE TABLE "Box" (
+	"id"	VARCHAR,
+	"prodotto"	INTEGER,
+	"qta"	INTEGER NOT NULL DEFAULT 0 CHECK("qta" >= 0),
+	FOREIGN KEY("prodotto") REFERENCES "Prodotto"("id") ON DELETE SET NULL ON UPDATE CASCADE,
+	PRIMARY KEY("id")
 );
 
-CREATE TABLE IF NOT EXISTS Ripiano (
-    id INTEGER,
-    scaffale INTEGER REFERENCES Scaffale(id),
-    corsia INTEGER REFERENCES Corsia(id),
-    PRIMARY KEY(id, scaffale, corsia)
+CREATE TABLE "Prodotto" (
+	"id"	INTEGER,
+	"nome"	VARCHAR NOT NULL,
+	"descrizione"	TEXT,
+	"peso"	REAL,
+	"soglia"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id")
 );
+
+CREATE TABLE "Ordine" (
+	"id"	INTEGER,
+	"tipo"	TEXT NOT NULL CHECK("tipo" IN ("IN", "OUT")),
+	"stato"	TEXT NOT NULL CHECK("stato" IN ("IN_ATTESA", "IN_SVOLGIMENTO", "COMPLETATO")),
+	"dataEm"	TEXT NOT NULL DEFAULT '1970-01-01' CHECK(dataEm LIKE '____-__-__'),
+	"dataCo"	TEXT CHECK(dataCo IS NULL OR dataCo LIKE '____-__-__'),
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "ProdottiOrdini" (
+	"prod"	INTEGER,
+	"ordine"	INTEGER,
+	"qta"	INTEGER NOT NULL CHECK("qta" > 0),
+	FOREIGN KEY("prod") REFERENCES "Prodotto"("id") ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY("ordine") REFERENCES "Ordine"("id") ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY("prod","ordine")
+);
+
+CREATE TABLE "Utente" (
+	"matricola"	TEXT,
+	"nome"	TEXT NOT NULL,
+	"cognome"	TEXT NOT NULL,
+	"pwdHash"	TEXT NOT NULL,
+	"ruolo"	TEXT NOT NULL CHECK(ruolo IN ("MAGAZZ", "MAGAZZ_QUAL", "RESP_ORDINI", "MANAGER")),
+	PRIMARY KEY("matricola")
+);
+
+CREATE TABLE "Movimentazione" (
+	"ordine"	INTEGER,
+	"prod"	INTEGER,
+	"box"	TEXT,
+	"qta"	INTEGER NOT NULL CHECK("qta" > 0),
+	"stato"	TEXT NOT NULL DEFAULT NON_ASSEGNATA CHECK("stato" IN ("NON_ASSEGNATA", "PRESA_IN_CARICO", "PRELEVATA", "COMPLETATA", "ANNULLATA")),
+	"magazziniere"	TEXT COLLATE BINARY,
+	FOREIGN KEY("box") REFERENCES "Box"("id") ON UPDATE CASCADE ON DELETE NO ACTION,
+	PRIMARY KEY("ordine","prod","box"),
+	FOREIGN KEY("prod") REFERENCES "Prodotto"("id") ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY("magazziniere") REFERENCES "Utente"("matricola") ON UPDATE CASCADE ON DELETE NO ACTION,
+	FOREIGN KEY("ordine") REFERENCES "Ordine"("id") ON UPDATE CASCADE ON DELETE CASCADE
+);
+
