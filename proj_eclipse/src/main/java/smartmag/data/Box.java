65d759c0a7b1efceff15fe9e5ddbb433a379c490
DavideBrambilla98 <d.brambilla7@studenti.unibg.it>
2024-02-24 09:39:19 +0100

    Aggiunti metodi equals e toString alla classe box

--- proj_eclipse/src/main/java/smartmag/data/Box.java
+++ proj_eclipse/src/main/java/smartmag/data/Box.java
@@ -1,5 +1,7 @@
 package smartmag.data;
 
+import java.util.Objects;
+
 public class Box {
 
 	private String indirizzo;
@@ -9,7 +11,7 @@ public class Box {
 	public Box(String indirizzo, int quantità, Prodotto prodotto) {
 		this.indirizzo = indirizzo;
 		this.quantità = quantità;
-		this.prodotto = prodotto;	
+		this.prodotto = prodotto;
 	}
 
 	public String getIndirizzo() {
@@ -36,15 +38,37 @@ public class Box {
 		this.quantità = quantità;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(indirizzo, prodotto, quantità);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Box other = (Box) obj;
+		return Objects.equals(indirizzo, other.indirizzo)
+				&& Objects.equals(prodotto, other.prodotto)
+				&& quantità == other.quantità;
+	}
+
+	@Override
+	public String toString() {
+		return "Box [indirizzo=" + indirizzo + ", prodotto=" + prodotto
+				+ ", quantità=" + quantità + "]";
+	}
+
 	public Boolean isValid() {
 		String[] parti = indirizzo.split("-");
-		if (parti.length == 3 && parti[0].matches("[a-zA-Z]+") && parti[1].matches("\\d+") && parti[2].matches("\\d+")) 
-			if ( this.quantità > 0 && this.prodotto != null )
+		if (parti.length == 3 && parti[0].matches("[a-zA-Z]+")
+				&& parti[1].matches("\\d+") && parti[2].matches("\\d+"))
+			if (this.quantità > 0 && this.prodotto != null)
 				return true;
 		return false;
 	}
-
 }
-
-
-
