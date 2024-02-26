78a1bbaf9bb1313df0a2922c880044fefad30601
DavideBrambilla98 <d.brambilla7@studenti.unibg.it>
2024-02-26 21:09:43 +0100

    Modifiche al modello per la gestione degli ordini
    - Aggiunti metodi toString agli enumeratori relativi agli ordini
    - Cancellata la classe ProductOrderModel poichè gestisco il record della tabella
       ProdottiOrdini direttamente nel modello "OrderModel"
    - Aggiunti alcuni dei metodi per la gestione dei record "ProdottiOrdini"

--- proj_eclipse/src/main/java/smartmag/data/StatoOrdine.java
+++ proj_eclipse/src/main/java/smartmag/data/StatoOrdine.java
@@ -6,4 +6,19 @@ package smartmag.data;
 public enum StatoOrdine {
 
 	IN_ATTESA, IN_SVOLGIMENTO, COMPLETATO;
+
+	@Override
+	public String toString() {
+		switch (this) {
+		case IN_ATTESA:
+			return "IN_ATTESA";
+		case IN_SVOLGIMENTO:
+			return "IN_SVOLGIMENTO";
+		case COMPLETATO:
+			return "COMPLETATO";
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
 }
--- proj_eclipse/src/main/java/smartmag/models/OrderModel.java
+++ proj_eclipse/src/main/java/smartmag/models/OrderModel.java
@@ -1,16 +1,21 @@
 package smartmag.models;
 
 import static ingsw_proj_magazzino.db.generated.Tables.ORDINE;
+import static ingsw_proj_magazzino.db.generated.Tables.PRODOTTIORDINI;
 import static org.jooq.impl.DSL.max;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import ingsw_proj_magazzino.db.generated.tables.records.OrdineRecord;
+import ingsw_proj_magazzino.db.generated.tables.records.ProdottiordiniRecord;
 import smartmag.data.Ordine;
+import smartmag.data.Prodotto;
 import smartmag.data.StatoOrdine;
 import smartmag.data.TipoOrdine;
 
@@ -20,6 +25,12 @@ public class OrderModel extends BaseModel {
 	private static HashMap<Integer, OrderModel> instances = new HashMap<Integer, OrderModel>();
 	private Ordine ordine;
 	private OrdineRecord orderRecord;
+	private ProdottiordiniRecord prodOrderRecord;
+	private ArrayList<ProdottiordiniRecord> prodottiRecord; // per inserire
+															// tutti i record
+															// dei prodotti nel
+															// caso dovesse
+															// servire
 
 	// Costruttore
 	private OrderModel(Ordine order) {
@@ -28,47 +39,47 @@ public class OrderModel extends BaseModel {
 	}
 
 	// aggiunte eccezione a causa delle conversioni
-	private void refresh() throws ParseException {
+	private void refreshOrderFromDb() throws ParseException {
 		this.orderRecord = fetchOrderById(ordine.getId());
 		if (this.orderRecord != null) {
 			Ordine o = ordineFromRecord(this.orderRecord);
-			this.orderRecord.setTipo(o.getTipo().name()); // aggiunto .name per
+			this.ordine.setTipo(o.getTipo());
 			// convertire
 			// ENUM->String
-			this.orderRecord.setStato(o.getStato().name()); // aggiunto .name
+			this.ordine.setStato(o.getStato()); // aggiunto .name
 			// per convertire
 			// ENUM->String
-			this.orderRecord.setDataem(o.getDataEmissione().toString());
-			this.orderRecord.setDataco(o.getDataCompletamento().toString());
+			this.ordine.setDataEmissione(o.getDataEmissione());
+			this.ordine.setDataCompletamento(o.getDataCompletamento());
 		}
 
 		// TODO: event
 	}
 
-	public boolean isSavedInDb() throws ParseException {
-		refresh();
+	public boolean orderIsSavedInDb() throws ParseException {
+		refreshOrderFromDb();
 		return orderRecord != null;
 	}
 
 	public void createOrdine()
 			throws SQLIntegrityConstraintViolationException, ParseException {
 
-		if (isSavedInDb())
+		if (orderIsSavedInDb())
 			throw new SQLIntegrityConstraintViolationException(
 					"Ordine #" + ordine.getId() + " esiste già!");
 
-		OrdineRecord r = DSL.newRecord(ORDINE);
-		r.setId(ordine.getId());
-		copyOrdineIntoRecord(ordine, r);
-		r.store(); // INSERT
-		this.orderRecord = r;
+		OrdineRecord or = DSL.newRecord(ORDINE);
+		or.setId(ordine.getId());
+		copyOrdineIntoRecord(ordine, or);
+		or.store(); // INSERT
+		this.orderRecord = or;
 
 		// TODO: event
 	}
 
 	public void updateOrdine(Ordine o)
 			throws SQLIntegrityConstraintViolationException, ParseException {
-		if (!isSavedInDb())
+		if (!orderIsSavedInDb())
 			throw new SQLIntegrityConstraintViolationException(
 					"Ordine #" + o.getId() + " non esiste!");
 
@@ -78,8 +89,39 @@ public class OrderModel extends BaseModel {
 	}
 
 	public void deleteOrdine() throws ParseException {
-		if (isSavedInDb()) {
+		if (orderIsSavedInDb()) {
 			orderRecord.delete(); // DELETE con UpdatableRecord
+		}
+	}
+
+	public void createProdottoOrdine() {
+		HashMap<Prodotto, Integer> prodotti = ordine.getProdotti();
+
+		for (Map.Entry<Prodotto, Integer> entry : prodotti.entrySet()) {
+			Prodotto p = entry.getKey();
+			int qta = entry.getValue();
+			ProdottiordiniRecord por = DSL.newRecord(PRODOTTIORDINI);
+			copyProdOrderIntoRecord(this.ordine, p, qta, por);
+			por.store();
+			this.prodOrderRecord = por;
+		}
+	}
+
+	public void updateProdottoOrdine(Ordine o, Prodotto p, int qta)
+			throws SQLIntegrityConstraintViolationException, ParseException {
+		if (!orderIsSavedInDb())
+			throw new SQLIntegrityConstraintViolationException(
+					"Ordine #" + o.getId() + " non esiste!");
+
+		prodOrderRecord = (ProdottiordiniRecord) fetchProductOrderByIds(
+				o.getId(), p.getId());
+		copyProdOrderIntoRecord(o, p, qta, prodOrderRecord);
+		prodOrderRecord.store(); // UPDATE con UpdatableRecord
+	}
+
+	public void deleteProductOrder() throws ParseException {
+		if (orderIsSavedInDb()) {
+			prodOrderRecord.delete(); // DELETE con UpdatableRecord
 		}
 	}
 
@@ -108,13 +150,24 @@ public class OrderModel extends BaseModel {
 		return r;
 	}
 
+	private static ProdottiordiniRecord fetchProductOrderByIds(int idO,
+			int idP) {
+		ProdottiordiniRecord r = (ProdottiordiniRecord) DSL.select()
+				.from(PRODOTTIORDINI)
+				.where(PRODOTTIORDINI.ORDINE.eq(idO)
+						.and(PRODOTTIORDINI.PROD.eq(idP)))
+				.fetchOne(); // SELECT
+		return r;
+	}
+
 	// restituisce un ordine partendo dal suo record
 	private static Ordine ordineFromRecord(OrdineRecord r)
 			throws ParseException {
 		if (r == null)
 			return null;
-		// c'è un controllo sull'inserimento corretto della data?
+
 		Integer id = r.getId();
+
 		TipoOrdine tipo = TipoOrdine.valueOf(r.getTipo());
 		StatoOrdine stato = StatoOrdine.valueOf(r.getStato());
 		Date dataEmissione = new SimpleDateFormat("dd/MM/yyy")
@@ -131,8 +184,15 @@ public class OrderModel extends BaseModel {
 		r.setDataco(o.getDataCompletamento().toString());
 	}
 
+	private static void copyProdOrderIntoRecord(Ordine o, Prodotto p, int qta,
+			ProdottiordiniRecord r) {
+		r.setOrdine(o.getId());
+		r.setProd(p.getId());
+		r.setQta(qta);
+	}
+
 	// incrementa id ordine
-	public static int getNextAvailableId() {
+	public static int getNextAvailableOrderId() {
 		Integer max = DSL.select(max(ORDINE.ID)).from(ORDINE).fetchOne()
 				.value1();
 		if (max == null)
