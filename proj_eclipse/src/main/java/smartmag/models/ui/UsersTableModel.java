3ed98de7881aee8f59ccec6f3e0b7a86002e0c0f
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2024-03-02 15:25:10 +0100

    Created UsersTableModel file, not implemented yet

---
+++ proj_eclipse/src/main/java/smartmag/models/ui/UsersTableModel.java
@@ -0,0 +1,29 @@
+package smartmag.models.ui;
+
+import javax.swing.table.AbstractTableModel;
+
+public class UsersTableModel extends AbstractTableModel {
+
+	public UsersTableModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
