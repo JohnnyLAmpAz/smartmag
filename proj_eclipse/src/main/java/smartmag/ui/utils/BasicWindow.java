2a59a751c6acf168e3a7a359fc491bb66d58e2cd
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2024-03-18 00:10:26 +0100

    New logo

--- proj_eclipse/src/main/java/smartmag/ui/utils/BasicWindow.java
+++ proj_eclipse/src/main/java/smartmag/ui/utils/BasicWindow.java
@@ -7,13 +7,23 @@ public class BasicWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Icona del logo
+	 */
+	public static final ImageIcon logo = new ImageIcon("./img/logo_alpha.png");
+
+	/**
+	 * Immagine con scritta logo
+	 */
+	public static final ImageIcon scrittaLogo = new ImageIcon(
+			"./img/smartmag.png");
+
 	public BasicWindow(String title, int x, int y, boolean resizable) {
 		this.setTitle(title);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(resizable);
 
 		// Set Icon
-		ImageIcon logo = new ImageIcon("img/icon.png");
 		this.setIconImage(logo.getImage());
 
 		this.setSize(x, y);
