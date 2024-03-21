9aa2918ba296fa6a1ab174858fe05ed447d3c373
DavideBrambilla98 <d.brambilla7@studenti.unibg.it>
2024-03-21 10:39:29 +0100

    Update OrderModelTest.java
    testati metodi:
    - create()
    - getOrderModelOf()
    - getOrdermodelById()
    -

--- proj_eclipse/src/test/java/smartmag/models/OrderModelTest.java
+++ proj_eclipse/src/test/java/smartmag/models/OrderModelTest.java
@@ -1,12 +1,14 @@
 package smartmag.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.jooq.exception.IntegrityConstraintViolationException;
@@ -19,6 +21,30 @@ import smartmag.data.StatoOrdine;
 import smartmag.data.TipoOrdine;
 
 class OrderModelTest extends BaseTest {
+
+	public static HashMap<Prodotto, Integer> prodottiStatic;
+	public static LocalDate demStatic;
+	public static LocalDate dcoStatic;
+
+	static {
+		demStatic = LocalDate.of(2024, 04, 04);
+		dcoStatic = LocalDate.of(2024, 02, 01);
+
+		Prodotto p = new Prodotto(ProductModel.getNextAvailableId(), "scala2",
+				"bassa", 10, 2);
+		Prodotto p1 = new Prodotto(ProductModel.getNextAvailableId() + 1,
+				"scala3", "alta", 100, 2);
+		Prodotto p2 = new Prodotto(ProductModel.getNextAvailableId() + 2,
+				"scala4", "bassa", 20, 3);
+		Prodotto p3 = new Prodotto(ProductModel.getNextAvailableId() + 3,
+				"scala5", "media", 30, 4);
+		prodottiStatic = new HashMap<>();
+		prodottiStatic.put(p, 2);
+		prodottiStatic.put(p1, 3);
+		prodottiStatic.put(p2, 4);
+		prodottiStatic.put(p3, 5);
+
+	}
 
 	@Test
 	void testGetOrdine() {
@@ -66,78 +92,105 @@ class OrderModelTest extends BaseTest {
 	}
 
 	@Test
-	void testUpdateQtaProdottoOrdine() {
-		fail("Not yet implemented");
+	void testUpdateQtaProdottoOrdine()
+			throws SQLIntegrityConstraintViolationException, ParseException {
+		Ordine o = new Ordine(OrderModel.getNextAvailableOrderId(),
+				TipoOrdine.OUT,
+				StatoOrdine.IN_ATTESA, demStatic, dcoStatic);
+		o.setProdotti(prodottiStatic);
+		OrderModel om = OrderModel.create(o);
+
+		ArrayList<Prodotto> p = new ArrayList<>();
+		om.getOrdine().getProdotti().forEach((t, u) -> p.add(t));
+		Prodotto pMod = p.getFirst();
+		// TODO non crea il record prodottoOrdini!!!
+		om.updateQtaProdottoOrdine(pMod, 222);
+
 	}
 
+	/**
+	 * verifica che il modello dell'ordine appena creato corrisponde a quello
+	 * che restituisc il metodo utilizzando solo l'id dell'ordine
+	 */
 	@Test
-	void testIsPreparabile() {
-		fail("Not yet implemented");
+	void testGetOrderModelById()
+			throws SQLIntegrityConstraintViolationException, ParseException {
+		Ordine o = new Ordine(OrderModel.getNextAvailableOrderId(),
+				TipoOrdine.OUT,
+				StatoOrdine.IN_ATTESA, demStatic, dcoStatic);
+		o.setProdotti(prodottiStatic);
+		OrderModel om = OrderModel.create(o);
+		Ordine o1 = new Ordine(OrderModel.getNextAvailableOrderId() + 1,
+				TipoOrdine.OUT,
+				StatoOrdine.IN_ATTESA, demStatic, dcoStatic);
+		o1.setProdotti(prodottiStatic);
+		OrderModel om1 = OrderModel.create(o1);
+
+		assertEquals(om, OrderModel.getOrderModelById(o.getId()));
+		assertNotEquals(om1, OrderModel.getOrderModelById(o.getId()));
 	}
 
+	/**
+	 * verifica che il modello dell'ordine appena creato corrisponde a quello
+	 * che restituisc il metodo
+	 */
 	@Test
-	void testProductOrderIsSavedInDb() {
-		fail("Not yet implemented");
+	void testGetOrderModelOf()
+			throws SQLIntegrityConstraintViolationException, ParseException {
+		Ordine o = new Ordine(OrderModel.getNextAvailableOrderId(),
+				TipoOrdine.OUT,
+				StatoOrdine.IN_ATTESA, demStatic, dcoStatic);
+		o.setProdotti(prodottiStatic);
+		OrderModel om = OrderModel.create(o);
+
+		assertEquals(om, OrderModel.getOrderModelOf(o));
+
 	}
 
-	@Test
-	void testGetOrderModelById() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	void testGetNextAvailableOrderId() {
+//		fail("Not yet implemented");
+//	}
 
-	@Test
-	void testGetOrderModelOf() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetNextAvailableOrderId() {
-		fail("Not yet implemented");
-	}
-
+	/**
+	 * Verifica il metodo create, prima con un ordine valido e poi con un ordine
+	 * non valido. Con l'ordine non valido, ci si aspetta un'eccezione
+	 * 
+	 * @throws SQLIntegrityConstraintViolationException
+	 * @throws ParseException
+	 */
 	@Test
 	void testCreate()
 			throws SQLIntegrityConstraintViolationException, ParseException {
 
-		LocalDate dem = LocalDate.parse("2024-03-05");
-		LocalDate dco = LocalDate.of(2024, 04, 04);
-		Prodotto p = new Prodotto(ProductModel.getNextAvailableId(), "scala2",
-				"bassa", 10, 2);
-		Prodotto p1 = new Prodotto(ProductModel.getNextAvailableId() + 1,
-				"scala3", "alta", 100, 2);
-		Prodotto p2 = new Prodotto(ProductModel.getNextAvailableId() + 2,
-				"scala4", "bassa", 20, 3);
-		Prodotto p3 = new Prodotto(ProductModel.getNextAvailableId() + 3,
-				"scala5", "media", 30, 4);
-		HashMap<Prodotto, Integer> prodotti = new HashMap<>();
-		prodotti.put(p, 2);
-		prodotti.put(p1, 3);
-		prodotti.put(p2, 4);
-		prodotti.put(p3, 5);
-
-		ProductModel.createProdotto(p);
-		ProductModel.createProdotto(p1);
-		ProductModel.createProdotto(p2);
-		ProductModel.createProdotto(p3);
-
 		Ordine o = new Ordine(OrderModel.getNextAvailableOrderId(),
 				TipoOrdine.OUT,
-				StatoOrdine.IN_ATTESA, dem, dco);
-		o.setProdotti(prodotti);
+				StatoOrdine.IN_ATTESA, demStatic, dcoStatic);
+		o.setProdotti(prodottiStatic);
 
 		OrderModel om = OrderModel.create(o);
 		/**
 		 * Verifica che l'ordine immesso sia presente nel DB
 		 */
 		assertEquals(om, OrderModel.getOrderModelOf(o));
+		/**
+		 * Deve lanciare un'eccezione poichè si cerca di creare un record di un
+		 * ordine già esistente
+		 */
+		assertThrows(SQLIntegrityConstraintViolationException.class,
+				() -> OrderModel.create(o));
+
 		Prodotto pnv = new Prodotto(ProductModel.getNextAvailableId() + 4,
 				"scala5", "media", 30, 4);
-		prodotti.put(pnv, 0);
+		prodottiStatic.put(pnv, 0);
 		ProductModel.createProdotto(pnv);
 		Ordine onv = new Ordine(OrderModel.getNextAvailableOrderId() + 1,
 				TipoOrdine.OUT,
-				StatoOrdine.IN_ATTESA, dem, dco);
-		onv.setProdotti(prodotti);
+				StatoOrdine.IN_ATTESA, demStatic, dcoStatic);
+		onv.setProdotti(prodottiStatic);
+		/*
+		 * verifica che venga lanciata un'eccezione se l'ordine non è valido
+		 */
 		assertThrows(IntegrityConstraintViolationException.class,
 				() -> OrderModel.create(onv));
 	}
@@ -149,7 +202,15 @@ class OrderModelTest extends BaseTest {
 
 	@Override
 	protected void postSetUp() {
-		// TODO Auto-generated method stub
+
+		prodottiStatic.forEach((t, u) -> {
+			try {
+				ProductModel.createProdotto(t);
+			} catch (SQLIntegrityConstraintViolationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
 
 	}
 
